83d28d2db371ec78686f710ba258c441031e75c8
Jmily <weiyi.li713@gmail.com>
2016-02-01 18:00:37 +1100

    improves dql

--- src/AppBundle/Entity/CategoryRepository.php
+++ src/AppBundle/Entity/CategoryRepository.php
@@ -13,11 +13,18 @@ class CategoryRepository extends EntityRepository
 {
     public function findAll()
     {
-        return $this->getEntityManager()
+        $query = $this->getEntityManager()
             ->createQuery(
-                'SELECT c FROM AppBundle:Category c ORDER BY c.priority ASC'
-            )
-            ->getResult();
+                'SELECT c,c1,p,b FROM AppBundle:Category c
+                 LEFT JOIN c.children c1
+                 LEFT JOIN c.products p
+                 LEFT JOIN p.brand b
+                ORDER BY c.priority ASC'
+            );
+
+        $result = $query->getResult();
+        return $result;
+
     }
 
 }
